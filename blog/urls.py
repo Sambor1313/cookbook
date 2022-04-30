c9fd995d38903be42a09760f1a909fae4296a261
Sambor1313 <sambor.maciejak@zhp.net.pl>
2022-05-01 00:51:49 +0200

    feature init site templates and structure

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,19 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('recipes', views.recipes, name='recipes'),
+    path('recipes/<int:recipe_id>', views.recipe, name='recipe'),
+
+    path('packages/', views.packages, name='packages'),
+    path('packages/<int:package_id>', views.package, name='package'),
+
+    path('foodpedia/', views.foodpedia, name='foodpedia'),
+    path('foodpedia/<int:ingredient_id>', views.foodpedia_article, name='foodpedia_article'),
+
+    path('value/', views.value, name='value'),
+
+    path('blog/', views.blog, name='blog'),
+    path('blog/<int:article_id>', views.blog_article, name='blog_post'),
+
+    path('search', views.search_list, name='search')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,44 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.paginator import Paginator
+
+from blog.models import BlogPost
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World")
+    return render(request, 'blog/index.html', context={})
+
+def recipes(request):
+    return render(request, 'blog/recipes.html', context={'test': ["a", "b", "c"]})
+
+def recipe(request, recipe_id):
+    return render(request, 'blog/recipe.html', context={})
+
+def packages(request):
+    return render(request, 'blog/packages.html', context={})
+
+def package(request, package_id):
+    return render(request, 'blog/package.html', context={})
+
+def foodpedia(request):
+    return render(request, 'blog/foodpedia.html', context={})
+
+def foodpedia_article(request, ingredient_id):
+    return render(request, 'blog/foodpedia_article.html', context={})
+
+def value(request):
+    return render(request, 'blog/value.html', context={})
+
+def blog(request):
+    blog_list = BlogPost.objects.all().order_by('publish_date')
+    paginator = Paginator(blog_list, 1)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'blog/blog.html', context={'page_obj': page_obj})
+
+def blog_article(request, article_id):
+    return render(request, 'blog/blog_article.html', context={})
+
+def search_list(request, fraze):
+    return render(request, 'blog/search_list.html', context={})
+
